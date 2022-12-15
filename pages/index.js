1648010eeda8ef92cc17efefb588ff89d676df33
davidktlee <rudxor567@naver.com>
2022-12-15 15:58:19 +0900

    chore: 철자 수정

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/Layout'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
